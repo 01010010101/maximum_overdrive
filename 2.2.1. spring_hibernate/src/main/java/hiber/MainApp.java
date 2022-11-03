b87ac681d69d1994615008d1e2a983ccc29cd3c7
Dunkelzahn <cccp010100101@gmail.com>
2022-11-03 22:51:02 +0400

    ver 1.00

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -0,0 +1,42 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+      userService.add(new User("Mikhail", "Boyarskiy", "1000ofimps@mail.ru"));
+      userService.add(new User("Dominik", "Torretto", "fastnfurious@mail.ru"));
+      userService.add(new User("Michael", "Schumacher", "user3@mail.ru"));
+      userService.add(new User("Johny", "Knoxwille", "user4@mail.ru"));
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println();
+      }
+
+      ArrayList<Car> cars = new ArrayList<>();
+      cars.add(new Car("Vaz", 2107));
+      for (Car car : cars) {
+         System.out.println("Id = "+car.getId());
+         System.out.println("Model = "+car.getModel());
+         System.out.println("Series = "+car.getSeries());
+      }
+      context.close();
+   }
+}
