01889aaf0499ca6a6856f95a9ebf9e45469291b6
Dunkelzahn <cccp010100101@gmail.com>
2022-11-07 00:32:29 +0400

    ver 1.3.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -33,7 +33,11 @@ public class UserDaoImp implements UserDao {
    @Override
    @SuppressWarnings("unchecked")
    public List<User> getUsersByACar(String model, int series) {
-      Car car = carDao.getCarByMS(model, series).get(0);
+      List<Car> cars = carDao.getCarByMS(model, series);
+      if (cars.size() == 0) {
+         return null;
+      }
+      Car car = cars.get(0);
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User where car_id = :car_id");
       query.setParameter("car_id", car.getId());
       return query.getResultList();
